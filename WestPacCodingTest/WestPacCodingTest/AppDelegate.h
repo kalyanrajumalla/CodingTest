7aa97b86c89dc8d1d4e8ce752ece3e7cc9427a27
Arepu PavanKumar <iosdeveloper.ipa@gmail.com>
2015-11-10 15:26:12 +0530

    Committing
    Submitting the code as a initial commit.

---
+++ WestPacCodingTest/WestPacCodingTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WestPacCodingTest
+//
+//  Created by Pavankumar Arepu on 10/11/2015.
+//  Copyright © 2015 TCS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
